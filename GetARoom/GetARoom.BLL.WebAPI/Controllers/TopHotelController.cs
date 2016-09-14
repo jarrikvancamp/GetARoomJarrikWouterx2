968f860f68c02a62c6be625332f13860b5baf905
Jarrik Van Camp <jarrik.vancamp@ordina.be>
2016-09-14 15:23:23 +0200

    TopHotels made

---
+++ GetARoom/GetARoom.BLL.WebAPI/Controllers/TopHotelController.cs
@@ -0,0 +1,22 @@
+﻿using GetARoom.BLL.WebAPI.Models.Helpers;
+using GetARoom.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace GetARoom.BLL.WebAPI.Controllers
+{
+    public class TopHotelController : ApiController
+    {
+        [EnableCors(origins: "*", headers: "*", methods: "*")]
+        public IEnumerable<Hotel> GetAll(int Id)
+        {
+            var hotels = HotelHelper.GetTopHotels(Id);
+            return hotels;
+        }
+    }
+}
